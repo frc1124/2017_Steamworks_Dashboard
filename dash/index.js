97d166dc07fef788d2fb9b8f4b86745f81846ea0
Shmish <shmish90@gmail.com>
2017-02-19 15:57:02 -0500

    update

--- dash/index.js
+++ dash/index.js
@@ -14,6 +14,8 @@ function sendAuto(id) { NetworkTables.putValue('/dash/auto', id); }
 
 //graph
 var canvas = document.getElementById('graph');
+canvas.width = document.getElementById('dataWrapper').clientWidth;
+canvas.height = document.getElementById('dataWrapper').clientHeight;
 var WIDTH = canvas.clientWidth;
 var HEIGHT = canvas.clientHeight;
 var ctx = canvas.getContext('2d');
@@ -40,20 +42,31 @@ var dataSets = {
     speed:[],
     pressure:[]
 }
-NetworkTables.addKeyListener('/dash/speed', function(key,value,isNew) { value.speed = value; });
-NetworkTables.addKeyListener('/dash/pressure', function(key,value,isNew) { value.pressure = pressure; });
+NetworkTables.addKeyListener('/dash/speed', function(key,value,isNew) { value.speed = HEIGHT - ((value / scales.speed)*HEIGHT); });
+NetworkTables.addKeyListener('/dash/pressure', function(key,value,isNew) { value.pressure = HEIGHT - ((value / scales.pressure)*HEIGHT); });
 
 setInterval(function() {
+    ctx.clearRect(0,0,WIDTH,HEIGHT);
     (dataSets.speed.length > WIDTH) ? dataSets.speed.splice(0,1) : null;
     (dataSets.pressure.length > WIDTH) ? dataSets.pressure.splice(0,1) : null;
-    for(point in dataSets.speed) { point.x--; }
-    for(point in dataSets.pressure) { point.x--; }
+    for(var point of dataSets.speed) { point.x--; }
+    for(var point of dataSets.pressure) { point.x--; }
+    dataSets.speed.push(new Point(value.speed));
+    dataSets.pressure.push(new Point(value.pressure));
     
     ctx.beginPath();
     ctx.moveTo(dataSets.speed[0].x, dataSets.speed[0].y);
     for(var i=0;i<dataSets.speed.length;i++) { ctx.lineTo(dataSets.speed[i].x,dataSets.speed[i].y); }
+    ctx.strokeStyle = key[0].color;
     ctx.stroke();
-}, 100);
+
+    ctx.beginPath();
+    ctx.moveTo(dataSets.pressure[0].x, dataSets.pressure[0].y);
+    for(var i=0;i<dataSets.pressure.length;i++) { ctx.lineTo(dataSets.pressure[i].x,dataSets.pressure[i].y); }
+    ctx.strokeStyle = key[1].color;
+    ctx.stroke();
+
+}, 50);
 function Point(y) {
     this.x = WIDTH;
     this.y = y;
