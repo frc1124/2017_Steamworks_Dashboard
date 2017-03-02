f210d73bfa99aa7ceff9f589e4d0b95f01f7d8f9
Shmish <shmish90@gmail.com>
2017-03-01 22:23:15 -0500

    more things

--- dash/index.js
+++ dash/index.js
@@ -1,3 +1,40 @@
+var key = [
+    {
+        name:"speed",
+        color:"#00FFFF"
+    },
+    {
+        name:"pressure",
+        color:"#FF8C00"
+    },
+    {
+        name:"time",
+        color:"#FF0000"
+    },
+    {
+        name:"battery",
+        color:"#00FF00"
+    }
+];
+var scales = {
+    speed:15,
+    pressure:120,
+    time:135,
+    battery:14
+}
+var value = {
+    speed:10,
+    pressure:30,
+    time:135,
+    battery:13
+}
+var dataSets = {
+    speed:[],
+    pressure:[],
+    time:[],
+    battery:[]
+}
+
 //status
 NetworkTables.addKeyListener('dash/status/comms', function() {}, true);
 NetworkTables.addKeyListener('dash/status/code', function() {}, true);
@@ -13,6 +50,7 @@ NetworkTables.addKeyListener('/dash/speed', function(key,value,isNew) {
     document.getElementById('speed').innerText = value;
     value.speed = HEIGHT - ((value / scales.speed)*HEIGHT);
  }, true);
+ NetworkTables.addKeyListener('/dash/centerLine', function(key,value,isNew) { centerLine = value / 640 * canvasTwo.width; });
 
 //autoSelect
 NetworkTables.addKeyListener('/dash/auto', function(key,value,isNew) {
@@ -30,39 +68,26 @@ var WIDTH = canvas.clientWidth;
 var HEIGHT = canvas.clientHeight;
 var ctx = canvas.getContext('2d');
 
-var key = [
-    {
-        name:"speed",
-        color:"#00FFFF"
-    },
-    {
-        name:"pressure",
-        color:"#FF8C00"
-    }
-];
-var scales = {
-    speed:15,
-    pressure:120
-}
-var value = {
-    speed:10,
-    pressure:30
-}
-var dataSets = {
-    speed:[],
-    pressure:[]
-}
+
 //NetworkTables.addKeyListener('/dash/speed', function(key,value,isNew) {  }, true);
 NetworkTables.addKeyListener('/dash/pressure', function(key,value,isNew) { value.pressure = HEIGHT - ((value / scales.pressure)*HEIGHT); });
+NetworkTables.addKeyListener('/dash/time', function(key,value,isNew) { value.time = HEIGHT - ((value / scales.time)*HEIGHT); });
+NetworkTables.addKeyListener('/dash/battery', function(key,value,isNew) { value.battery = HEIGHT - ((value / scales.battery)*HEIGHT); });
 
 setInterval(function() {
     ctx.clearRect(0,0,WIDTH,HEIGHT);
     (dataSets.speed.length > WIDTH) ? dataSets.speed.splice(0,1) : null;
     (dataSets.pressure.length > WIDTH) ? dataSets.pressure.splice(0,1) : null;
+    (dataSets.time.length > WIDTH) ? dataSets.time.splice(0,1) : null;
+    (dataSets.battery.length > WIDTH) ? dataSets.battery.splice(0,1) : null;
     for(var point of dataSets.speed) { point.x--; }
     for(var point of dataSets.pressure) { point.x--; }
+    for(var point of dataSets.time) { point.x--; }
+    for(var point of dataSets.battery) { point.x--; }
     dataSets.speed.push(new Point(value.speed));
     dataSets.pressure.push(new Point(value.pressure));
+    dataSets.time.push(new Point(value.time));
+    dataSets.battery.push(new Point(value.battery));
     
     ctx.beginPath();
     ctx.moveTo(dataSets.speed[0].x, dataSets.speed[0].y);
@@ -76,8 +101,36 @@ setInterval(function() {
     ctx.strokeStyle = key[1].color;
     ctx.stroke();
 
+    ctx.beginPath();
+    ctx.moveTo(dataSets.time[0].x, dataSets.time[0].y);
+    for(var i=0;i<dataSets.time.length;i++) { ctx.lineTo(dataSets.time[i].x,dataSets.time[i].y); }
+    ctx.strokeStyle = key[2].color;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(dataSets.battery[0].x, dataSets.battery[0].y);
+    for(var i=0;i<dataSets.battery.length;i++) { ctx.lineTo(dataSets.battery[i].x,dataSets.battery[i].y); }
+    ctx.strokeStyle = key[3].color;
+    ctx.stroke();
+
 }, 50);
 function Point(y) {
     this.x = WIDTH;
     this.y = y;
 }
+
+
+
+var centerLine;
+var canvasTwo = document.getElementById('canvasTwo');
+canvasTwo.width = canvasTwo.clientWidth;
+canvasTwo.height = canvasTwo.clientHeight;
+var ctxTwo = canvasTwo.getContext('2d');
+setInterval(function() {
+    ctxTwo.clearRect(0,0, canvasTwo.width,canvasTwo.height);
+    ctxTwo.beginPath();
+    ctxTwo.moveTo(centerLine,0);
+    ctxTwo.lineTo(centerLine,canvasTwo.height);
+    ctxTwo.strokeStyle = '#FF0000';
+    ctxTwo.stroke();
+},50)
